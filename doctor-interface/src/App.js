fd059fc26d98bb68263d4e26b4094bc272b2e1cf
sigma67 <benedikt.putz@stud.uni-regensburg.de>
2019-08-02 15:47:21 +0200

    Add drugs sample data to doctor/patient interface

--- doctor-interface/src/App.js
+++ doctor-interface/src/App.js
@@ -11,6 +11,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import QRModal from './QRModal'
 import './App.css';
+import drugs from './drugs.json';
+import Autocomplete from 'react-autocomplete';
 import 'font-awesome/css/font-awesome.min.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -44,7 +46,7 @@ class ModalForm extends Component {
   async sendPrescription() {
     let tx = await this.props.state.ContractInstance.prescribe(
       this.state.formState["patient-address"],
-      this.state.formState["pzn"],
+      this.state.pzn,
       this.state.formState["medication-name"],
       this.state.formState["dosage-quantity"],
       this.state.formState["dosage-unit"],
@@ -75,7 +77,7 @@ class ModalForm extends Component {
       this.state.transactionId = false;
       this.state.id = this.props.input.id;
       this.state.formState["patient-address"] = this.props.input.patientWalletAddress;
-      this.state.formState["pzn"] = this.props.input.pzn;
+      this.state.pzn = this.props.input.pzn;
       this.state.formState["medication-name"] = this.props.input.medicationName;
       this.state.formState["dosage-quantity"] = this.props.input.dosage;
       this.state.formState["dosage-unit"] = this.props.input.dosageUnit;
@@ -98,13 +100,38 @@ class ModalForm extends Component {
         <ModalBody>
           <Form onInput={this.inputUpdate.bind(this)} tooltip="">
             <FormGroup>
-              <Label for="exampleEmail">Patient wallet address:</Label>
+              <Label for="exampleEmail">Patient wallet address</Label>
               <Input type="text" name="patient-address"  value={this.state.formState["patient-address"] || ""} placeholder="0x123f681646d4a755815f9cb19e1acc8565a0c2ac" required valid invalid={!window.web3.isAddress(this.state.formState["patient-address"])}/>
               <FormFeedback>Not a valid address</FormFeedback>
             </FormGroup>
             <FormGroup>
               <Label for="exampleEmail">PZN</Label>
-              <Input type="text" name="pzn" value={this.state.formState["pzn"] || ""} required/>
+              <Autocomplete
+                getItemValue={(item) => item.label}
+                items={ drugs.map(function(drug){
+                  return {
+                    label : drug.PZN.toString() + " " + drug.Medikament,
+                    value: drug.PZN.toString()
+                  }; }) }
+                shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                getItemValue={item => item.value}
+                wrapperStyle={{}}
+                renderItem={(item, highlighted) =>
+                  <div
+                    key={item.id}
+                    style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+                  >
+                    {item.label}
+                  </div>
+                }
+                type="text"
+                name = "pzn"
+                inputProps={{className: "form-control"}}
+                required
+                value={this.state.pzn || ""}
+                onChange={e => this.setState({pzn : e.target.value} )}
+                onSelect={value => this.setState({pzn : value})}
+              />
             </FormGroup>
             <FormGroup>
               <Label for="exampleEmail">Medication Name</Label>
@@ -166,8 +193,9 @@ class App extends Component {
     this.state.accounts = accounts;
     this.state.ContractInstance = instance;
     await this.getPrescriptions();
-    //let doctor = await this.state.ContractInstance.approvedDoctors(this.state.accounts[0]);
-    //this.state.user = doctor.name;
+    let doctor = await this.state.ContractInstance.approvedDoctors(this.state.accounts[0]);
+    this.setState({user: doctor.name});
+    console.log(doctor.name);
     //this.forceUpdate()
   }
 
@@ -250,7 +278,7 @@ class App extends Component {
             <Media>
               <FontAwesome className="doctor-icon" object name='user-md' alt="User" size={"5x"}/>
               <Media body>
-                <h1>Hello, {this.state.user}</h1>
+                <h1>Hello {this.state.user},</h1>
                 <h4>Your recent prescriptions.</h4>
                 <code>{this.state.accounts[0]}</code>
               </Media>
--- patient-interface/src/App.js
+++ patient-interface/src/App.js
@@ -12,6 +12,7 @@ import PropTypes from 'prop-types';
 import QRModal from './QRModal';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import drugs from './drugs.json';
 import 'font-awesome/css/font-awesome.min.css';
 import {
   Media, Table, Button,
@@ -90,6 +91,46 @@ class QRAddressModal  extends Component {
   }
 }
 
+class DrugModal  extends Component {
+  constructor(props) {
+    super(props);
+    let drug = drugs.filter(f => f.PZN == this.props.pzn);
+    if(drug.length > 0){
+      drug = drug[0];
+    }
+    this.state = {
+      drug: drug
+    };
+  }
+  render () {
+    return (
+      <Modal isOpen={this.props.visibility} toggle={this.props.toggle}>
+        <ModalHeader toggle={this.props.toggle}>Prescription Information</ModalHeader>
+        <ModalBody>
+          <p><strong>Medikament</strong><br/>
+            {this.state.drug['Medikament']}</p>
+          <p><strong>Firma</strong><br/>
+            {this.state.drug['Firma']}</p>
+          <p><strong>Hinweise</strong><br/>
+            {this.state.drug['Hinweise']}</p>
+          <p><strong>Zusammensetzung</strong><br/>
+            {this.state.drug['Zusammensetzung (Zus.):']}</p>
+          <p><strong>Packungsangaben</strong><br/>
+            {this.state.drug['Packungsangaben']}</p>
+          <p><strong>AVP/UVP</strong><br/>
+            {this.state.drug['AVP/UVP (EUR)']} EUR</p>
+          <p><strong>PZN</strong><br/>
+            {this.state.drug['PZN']}</p>
+          <p><strong>Anwendungsgebiete</strong><br/>
+            {this.state.drug['Anwendungsgebiete']}</p>
+          <p><strong>Dosierung</strong><br/>
+            {this.state.drug['Dosierung']}</p>
+        </ModalBody>
+      </Modal>
+    )
+  }
+}
+
 ModalForm.contextTypes = {
   web3: PropTypes.object
 };
@@ -106,6 +147,7 @@ class App extends Component {
     this.toggle = this.toggle.bind(this);
     this.toggleQR = this.toggleQR.bind(this);
     this.toggleQRAddress = this.toggleQRAddress.bind(this);
+    this.toggleDrug = this.toggleDrug.bind(this);
   }
 
   async componentDidMount() {
@@ -131,7 +173,8 @@ class App extends Component {
       medicationName: f.metadata.medicationName,
       dosage: f.metadata.dosage,
       dosageUnit: f.metadata.dosageUnit,
-      filled: f.filled
+      filled: f.filled,
+      pzn: f.metadata.pzn
     };
   }
 
@@ -147,6 +190,15 @@ class App extends Component {
     this.setState({modalQRAddress: !this.state.modalQRAddress});
   }
 
+  showDrug(pzn){
+    console.log(pzn)
+    this.setState({pzn: pzn});
+    this.toggleDrug()
+  }
+
+  toggleDrug() {
+    this.setState({modalDrug: !this.state.modalDrug});
+  }
 
   saveAddress(address){
     this.state.address = address;
@@ -167,7 +219,7 @@ class App extends Component {
     return (
       <tr key={tx.id}>
         <td>{tx.id}</td>
-        <td>{tx.dosage}{tx.dosageUnit} of {tx.medicationName}</td>
+        <td onClick={ ()=> { this.showDrug(tx.pzn) }}>{tx.dosage}{tx.dosageUnit} of {tx.medicationName}</td>
         <td>{new Date(tx.expiryTime).toLocaleDateString("en-US")}</td>
         <td>{new Date(tx.prescribedAt).toLocaleDateString("en-US")}</td>
         <td>
@@ -187,19 +239,19 @@ class App extends Component {
         <a href="http://trio.bayern" target="_blank"><Media object src="./logo.svg" style={{ marginRight: 15 }} height="30px" align="right"/></a>
         <hr />
         <div className="row  position-relative">
-          <div className="col-md-6">
+          <div className="col-md-8">
             <Media>
               <FontAwesome className="user-icon clickable" onClick={() => { this.toggleQRAddress() }} name='user-circle' alt="User" size={"5x"}/>
               <Media body>
                 <h1>Hello,</h1>
                 <h4>You've recently been prescribed.</h4>
                 { this.state.accounts[0] !== undefined &&
-                  <p>Tap your profile icon to show your account address.</p>
+                  <p>Tap your profile icon to show your account address. <br />Tap the prescription to show medication information.</p>
                 }
               </Media>
             </Media>
           </div>
-          <div className="col-md-6 text-right position-absolute" style={{bottom: 0, right: 0}}>
+          <div className="col-md-4 text-right position-absolute" style={{bottom: 0, right: 0}}>
             <Button color="secondary" className="m-1" onClick= { ()=> { this.toggleQR() }}><FontAwesome name='camera' className="mr-2"/> Scan pharmacy address</Button>
           </div>
         </div>
@@ -222,6 +274,7 @@ class App extends Component {
         <ModalForm visibility={this.state.modal} toggle={this.toggle} state={this.state}/>
         <QRAddressModal visibility={this.state.modalQRAddress} toggle={this.toggleQRAddress} account={this.state.accounts[0]}/>
         <QRModal visibility={this.state.modalQR} toggle={this.toggleQR} state={this.state} onScan={this.saveAddress}/>
+        <DrugModal visibility={this.state.modalDrug} toggle={this.toggleDrug} pzn={this.state.pzn}/>
       </div>
     );
   }
