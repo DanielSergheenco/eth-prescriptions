3e07d174e0443f5848febc126336fec30cbf1880
DanielSergheenco <61294125+DanielSergheenco@users.noreply.github.com>
2022-08-27 03:53:42 +0300

    Update medici calificati

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,10 @@ const contract = artifacts.require("PrescriptionNFT");
 module.exports = function(deployer, network, accounts) {
     if(network == 'development') {
         deployer.deploy(contract,
-          [accounts[4], accounts[5]],
-          ["Dr. med. Bob Smith", "Dr. med. Michael Brown"]);
+          [accounts[1], accounts[2]],         //S-a specificat primele 2 adrese cunoscute in reteaua locala ganache
+          ["Prof. Dr. Catalin Popescu", "Prof. Dr. Ioana Achim"]);
     }
     else {
-        deployer.deploy(contract, ["0xe95f524fbe1443c2cfeEBE60bF4a6B17BE0f0D72"], ["Dr. Hofer"])
+        deployer.deploy(contract, ["0xe95f524fbe1443c2cfeEBE60bF4a6B17BE0f0D72"], ["Dr. Popescu"])
     }
 };
